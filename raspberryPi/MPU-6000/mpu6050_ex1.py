5c7fba68c9d8e17ad749f7fda11f773431c917bc
kyh <kyhoon001@naver.com>
2020-05-18 22:31:27 +0900

    mpu

--- raspberryPi/MPU-6000/mpu6050_ex1.py
+++ raspberryPi/MPU-6000/mpu6050_ex1.py
@@ -1,79 +1,56 @@
 from smbus2 import SMBus
 from bitstring import Bits
 import math
-
+import time
 bus = SMBus(1)
 DEV_ADDR = 0x68
-
-#GYRO
 register_gyro_xout_h = 0x43
 register_gyro_yout_h = 0x45
 register_gyro_zout_h = 0x47
 
-#GYTO LSB Sensitivity LSB/dps
 sensitive_gyro = 131.0
 
-# ACCELEROMETER
 register_accel_xout_h = 0x3B
-register_accel_yout_h = 0x3d
+register_accel_yout_h = 0x3D
 register_accel_zout_h = 0x3F
 
 sensitive_accel = 16384.0
 
 def read_data(register):
-    """
-    16 bit data from sensor measurement
-    """
     high = bus.read_byte_data(DEV_ADDR,register)
     low = bus.read_byte_data(DEV_ADDR,register+1)
     val = (high << 8) + low
     return val
 
-
-def twocomplements(val) :
-    """
-    2dml qhtncjfl translate to korean
-    """
-    s = Bits(unit=val, length= 16)
-    return s.int # signed 2's complement representation
-
-
-def gyro_dps(val) :
+def twocomplements(val):
+    s = Bits(uint=val,length=16)
+    return s.int
+def gyro_dps(val):
     return twocomplements(val)/sensitive_gyro
-
-def accel_g(val) :
+def accel_g(val):
     return twocomplements(val)/sensitive_accel
-
-def dist(a,b) :
-    return math.sqrt((a*b) + (b*b))
-
-def get_x_rotation(x,y,z) :
+def dist(a,b):
+    return math.sqrt((a*a)+(b*b))
+def get_x_rotation(x,y,z):
     radians = math.atan(x/dist(y,z))
     return radians
-
-def get_y_rotation(x,y,z) :
+def get_y_rotation(x,y,z):
     radians = math.atan(y/dist(x,z))
     return radians
-
-#reset
-bus.write_byte_data(DEV_ADDR,0x6B, 0b00000000)
-
-try :
+bus.write_byte_data(DEV_ADDR,0x6B,0b00000000)
+try:
     while True:
         x = read_data(register_accel_xout_h)
         y = read_data(register_accel_yout_h)
         z = read_data(register_accel_zout_h)
-
         aX = get_x_rotation(accel_g(x),accel_g(y),accel_g(z))
         aY = get_y_rotation(accel_g(x),accel_g(y),accel_g(z))
-        data = str(aX) + ',' + str(aY) + '$'
+        data = str(aX) + ' , ' + str(aY) + '$'
         print(data)
-
+        time.sleep(0.3)
 except KeyboardInterrupt:
     print("\nInterrupted!")
-
 except:
     print("\nClosing socket")
-
 finally:
     bus.close()
